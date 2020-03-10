7a6e6a7ca9e836c3da110372a45e1ce321d0f123
rfavaro <rodrigo@saudebig.com.br>
2020-03-10 15:38:07 -0300

    primeiro

---
+++ src/MediaSeisNumeros.ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MediaSeisNumeros.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listaNumeros = new List<double>();
+            for (int i = 0; i < 4; i++)
+            {
+               var numeroRequisitado = i + 1;     
+               Console.WriteLine($"Digite a nota do Bimestre {numeroRequisitado}:");
+               var numero = Console.ReadLine();
+               double.TryParse(numero, out var numerodouble);
+
+               listaNumeros.Add(numerodouble);  
+            }
+
+            Console.WriteLine("Digite a quantidade de aulas: ");
+            var total_aulas = Console.ReadLine();
+            
+            Console.WriteLine("Digite a quantidade total de presença do aluno: ");
+            var total_presenca = Console.ReadLine();
+
+            double perc_presenca = (double.Parse(total_presenca) / double.Parse(total_aulas)) * 100;
+            
+            var media = listaNumeros.Average();        
+
+            string aprovadoReprovado = "";
+            if ((media >= 5) && (perc_presenca >= 75))
+                aprovadoReprovado = "Aprovado";
+            else 
+                aprovadoReprovado = "Reprovado";
+
+           Console.WriteLine($"O Aluno foi {aprovadoReprovado} com a média: {Math.Round(media, 2)} e percentual de presenca de: {Math.Round(perc_presenca, 2)}");
+
+        }
+    }
+}
